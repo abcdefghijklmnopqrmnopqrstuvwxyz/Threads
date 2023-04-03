586608d26e0ba39fdf992356bf51c701e7addc3f
Erik Boháč <bohac2@SPSEJECNANET.CZ>
2023-04-03 08:16:30 +0200

    Push
    Test - threads

---
+++ Fronta.cs
@@ -0,0 +1,43 @@
+﻿namespace TestVlakna
+{
+    internal class Fronta
+    {
+        private static List<int> _seznam = new();
+        private static readonly object _lock = new();
+
+        public Fronta()
+        {
+        }
+
+        public void Vloz(int a)
+        {
+            lock (_lock)
+            {
+                _seznam.Add(a);
+            }
+        }
+
+        public int Ziskej(int b)
+        {
+            lock (_lock)
+            {
+                if (_seznam.Count > 0)
+                {
+                    int y = _seznam[b];
+                    _seznam.RemoveAt(b);
+                    return y;
+                }
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            if (_seznam.Count != 0)
+                return _seznam.Select(x => x.ToString()).Aggregate((a, b) => a + ", " + b);
+            else
+                return "List je prázdný.";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace TestVlakna
+{
+    internal class Program
+    {
+        static Fronta Fronta = new();
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(new ThreadStart(Pridej));
+            Thread thread2 = new Thread(new ThreadStart(Odeber));
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine(Fronta.ToString());
+        }
+
+        private static void Pridej()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Fronta.Vloz(random.Next(10) + 1);
+            }
+        }
+
+        private static void Odeber()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Fronta.Ziskej(0);
+            }
+        }
+
+    }
+}
